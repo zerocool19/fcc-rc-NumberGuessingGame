333b6d67a32dc8b6a250666444cf244b06c1044f
Davide <153355023+zerocool19@users.noreply.github.com>
2024-05-19 14:35:13 +0200

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -5,13 +5,35 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 
+REGISTRATION()
+{
+  if $NEW_USER
+  then
+    # New user registration
+    BEST_GAME_RESULT=$INPUT_COUNTER
+    REGISTRATION_RESULT=$($PSQL "INSERT INTO game_data(username, games_played, best_game) VALUES('$USERNAME_RESULT', $GAMES_PLAYED_RESULT, $BEST_GAME_RESULT)")
+  else
+    # Old user data update if needed
+    if [[ $INPUT_COUNTER < $BEST_GAME_RESULT ]]
+    then
+      # If old user did a better score
+      BEST_GAME_RESULT=$INPUT_COUNTER
+      UPDATE_RESULT=$($PSQL "UPDATE game_data SET games_played = $GAMES_PLAYED_RESULT, best_game = $BEST_GAME_RESULT WHERE username = '$USERNAME_RESULT'")
+    else
+      # If old user did a equal or worse score
+      UPDATE_RESULT=$($PSQL "UPDATE game_data SET games_played = $GAMES_PLAYED_RESULT WHERE username = '$USERNAME_RESULT'")
+    fi
+  fi
+}
+
+
 GUESSING_GAME()
 {
   # Random number generated for the while loop. Loop will terminate when user will give the correct answer
   INPUT=""
   INPUT_COUNTER=0
   RANDOM_NUMBER=$((1 + RANDOM % 1000))
-  echo $RANDOM_NUMBER
+  #echo $RANDOM_NUMBER
 
   while [[ $INPUT != $RANDOM_NUMBER ]]
   do
@@ -23,23 +45,7 @@ GUESSING_GAME()
     then
       # If input is the correct number
       echo "You guessed it in $INPUT_COUNTER tries. The secret number was $RANDOM_NUMBER. Nice job!"
-
-      # Logic to store best game result in database
-      if [[ $GAMES_PLAYED_RESULT == 1 ]]
-      then
-        # New user will have only 1 best game (the current)
-        BEST_GAME_RESULT=$INPUT_COUNTER
-        REGISTRATION
-      
-      else
-        # Comparing previous score with new one
-        if [[ $INPUT_COUNTER < $GAMES_PLAYED_RESULT ]]
-        then
-          # Actual score is better than previous one
-          BEST_GAME_RESULT=$INPUT_COUNTER
-          REGISTRATION
-        fi
-      fi
+      REGISTRATION
 
       else
         if [[ $INPUT > $RANDOM_NUMBER ]]
@@ -52,13 +58,6 @@ GUESSING_GAME()
         fi
       fi
   done
-}
-
-
-REGISTRATION()
-{
-  # Store game stats in database
-  REGISTRATION_RESULT=$($PSQL "INSERT INTO game_data(username, games_played, best_game) VALUES('$USERNAME_RESULT', $GAMES_PLAYED_RESULT, $BEST_GAME_RESULT)")
 }
 
 
@@ -75,13 +74,16 @@ else
   then
     # Username not found 
     echo "Welcome, $1! It looks like this is your first time here."
+    NEW_USER=true
     USERNAME_RESULT=$1
     GAMES_PLAYED_RESULT=1
   else
     # Username found
-    GAMES_PLAYED_RESULT=$(($PSQL "SELECT games_played FROM game_data WHERE username = $1") + 1)
-    BEST_GAME_RESULT=$($PSQL "SELECT best_game FROM game_data WHERE username = $1")
-    echo "Welcome back, '$USERNAME_RESULT'! You have player $GAMES_PLAYED_RESULT, and your best game took $BEST_GAME_RESULT guesses."
+    NEW_USER=false
+    GAMES_PLAYED_RESULT=$($PSQL "SELECT games_played FROM game_data WHERE username = '$1'")
+    ((GAMES_PLAYED_RESULT++))
+    BEST_GAME_RESULT=$($PSQL "SELECT best_game FROM game_data WHERE username = '$1'")
+    echo "Welcome back, $USERNAME_RESULT! You have played $GAMES_PLAYED_RESULT, and your best game took $BEST_GAME_RESULT guesses."
   fi
 
   GUESSING_GAME
